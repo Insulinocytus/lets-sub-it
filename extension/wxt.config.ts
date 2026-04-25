28b2fdbe7e4552684193dd419ee7f65e255de20f
Insulinocytus <171487220+Insulinocytus@users.noreply.github.com>
2026-04-25 14:15:44 +0900

    feat(extension): scaffold WXT + Vue + Tailwind + shadcn-vue project

---
+++ extension/wxt.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'wxt'
+
+export default defineConfig({
+  srcDir: 'src',
+  extensionApi: 'chrome',
+  modules: [],
+  manifest: {
+    name: "Let's Sub It",
+    version: '0.1.0',
+    permissions: ['storage', 'tabs'],
+    host_permissions: [
+      'http://localhost:8080/*',
+      'https://www.youtube.com/*',
+    ],
+    action: {
+      default_title: "Let's Sub It",
+      default_popup: 'entrypoints/popup/index.html',
+    },
+  },
+})
